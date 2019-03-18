7f34f68ec0ff229189017ed43af911e4a9bc3b87
Andrey <andreysvyatoy@gmil.com>
2019-03-18 14:20:10 +0300

    base lifecicle of smtp sender

---
+++ SmtpComponent/SmtpComponent.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace SmtpSender
+{
+    //TODO add logger or openspan logger
+
+    public class SmtpComponent
+    { 
+        private SmtpClient smtp { get; set; }
+        //TODO use autentification tocken of encripted credentials
+        private NetworkCredential creds { get; set; }
+
+
+        public MailMessage mail { get; set; }
+        public string username { get; set; }
+        public string password { get; set; } //TODO use secure string for password
+        public string host { get; set; }
+        public int port { get; set; }
+        public bool enableSsl { get; set; }
+
+
+        public SmtpComponent() { }
+
+        public void send()
+        {
+            initSmtp();
+            smtp.Send(mail);
+        }
+
+        public void send(string from, string to, string subject, string body) {
+            initSmtp();
+            smtp.Send(from, to, subject, body);
+        }
+
+        private void initSmtp() {
+            smtp = new SmtpClient(host: host, port: port);
+            creds = new NetworkCredential(password: password, userName: username);
+            smtp.Credentials = creds;
+            smtp.EnableSsl = enableSsl;
+        }
+
+        
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using SmtpSender;
+using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            SmtpComponent component = new SmtpComponent();
+            component.enableSsl = true;
+            component.host = "smtp.gmail.com";
+            component.port = 587;
+            component.username = "testcsharpsmtp@gmail.com";
+            component.password = "csharpsendertest";
+            component.enableSsl = true;
+            component.send(component.username, "andrey.svyatogorov@masterdata.ru", "test mess", "Text should be here, but it is test");
+
+            Console.ReadKey();
+        }
+    }
+}
