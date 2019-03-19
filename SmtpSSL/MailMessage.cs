1f95fb6846f3f2137c824400cd4d1e54ab1a120e
Andrey <andreysvyatoy@gmil.com>
2019-03-19 12:44:19 +0300

    add mail message component

---
+++ SmtpSSL/MailMessage.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Mail;
+using System.ComponentModel;
+
+namespace SmtpComponent
+{
+    public partial class MailMessage : Component
+    {
+        public MailMessage()
+        {
+            InitializeComponent();
+        }
+        public MailMessage(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        public System.Net.Mail.MailMessage mailMessage {  get; set; }
+    }
+}
