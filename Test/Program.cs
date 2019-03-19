5634a91e238c631b479ff57202aed7c76df71e19
Andrey <andreysvyatoy@gmil.com>
2019-03-19 12:51:55 +0300

    add test project

---
+++ Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security;
+using SmtpComponent;
+
+namespace Test
+{
+    class Program
+    {
+
+        static (string, string)[] test { get; set; }
+
+        static void Main(string[] args)
+        { 
+
+            SecureString sString; ;
+            unsafe
+            {
+                fixed (char* p = Console.ReadLine())
+                {
+                    sString = new SecureString(p, 10);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
